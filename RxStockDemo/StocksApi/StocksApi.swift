e750de53b42de72f7ea43f3a19c63228733e9a79
Frank <frank7300228@gmail.com>
2017-11-09 17:31:39 +0800

    Add StocksApi readme comments

--- RxStockDemo/StocksApi/StocksApi.swift
+++ RxStockDemo/StocksApi/StocksApi.swift
@@ -6,4 +6,8 @@
 //  Copyright © 2017 Frank. All rights reserved.
 //
 
+// Read me first:
+// To own your api keys, please go to: https://www.alphavantage.co/ to claim as many keys as you want (because each key has its frequency limitations).
+// You also need to create an array holding api keys - apiKeys: [String] during api services calling.
+
 import Foundation
