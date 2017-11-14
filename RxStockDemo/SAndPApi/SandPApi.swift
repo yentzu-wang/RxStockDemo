694a17b05c2ce81bd2db40d6dee46396b227f18a
Frank <frank7300228@gmail.com>
2017-11-14 13:41:25 +0800

    Add func parseDataToRealm()

--- RxStockDemo/SAndPApi/SandPApi.swift
+++ RxStockDemo/SAndPApi/SandPApi.swift
@@ -9,6 +9,7 @@
 import Foundation
 import RxSwift
 import RxAlamofire
+import RealmSwift
 
 struct Symbol: Codable {
     let name: String?
@@ -29,7 +30,7 @@ protocol SandPApiProtocol {
 final class SandPApi: SandPApiProtocol {
     static let shared = SandPApi()
     private let symbolUrl = "https://pkgstore.datahub.io/core/s-and-p-500-companies:constituents_json/data/constituents_json.json"
-    
+    private let bag = DisposeBag()
     private init() {}
     
     func getSymbols() -> Observable<[Symbol]> {
@@ -45,6 +46,33 @@ final class SandPApi: SandPApiProtocol {
                     return [Symbol]()
                 }
         }
+    }
+    
+    func parseDataToRealm() {
+        _ = getSymbols()
+            .subscribe(onNext: { (symbols) in
+                _ = symbols.map({ symbol in
+                    let realm = try! Realm()
+                    let stockSymbol = realm.object(ofType: StockSymbol.self, forPrimaryKey: symbol.symbol!)
+                    
+                    if stockSymbol == nil {
+                        let stockSymbol = StockSymbol()
+                        stockSymbol.symbol = symbol.symbol!
+                        stockSymbol.name = symbol.name!
+                        stockSymbol.sector = symbol.sector!
+                        
+                        try! realm.write {
+                            realm.add(stockSymbol)
+                        }
+                    } else if let stockSymbol = stockSymbol {
+                        try! realm.write {
+                            stockSymbol.name = symbol.name!
+                            stockSymbol.sector = symbol.sector!
+                        }
+                    }
+                })
+            })
+            .disposed(by: bag)
     }
 }
 
