8362e6797d0dcceb920032488ae9c4f977d79452
Frank <frank7300228@gmail.com>
2017-11-19 23:57:27 +0800

    Update test cases

--- RxStockDemoTests/SAndPApi/SandPApiTests.swift
+++ RxStockDemoTests/SAndPApi/SandPApiTests.swift
@@ -20,8 +20,8 @@ class SandPApiTests: XCTestCase {
     var testObj: [Symbol]!
     let foo1 = Symbol(name: "foo1", sector: "foo1", symbol: "foo1")
     
-    let url = "api.example.com"
-    let fakeUrl = "fake.url.com"
+    let request = URLRequest(url: URL(string: "www.SandP.com")!)
+    let errorRequest = URLRequest(url: URL(string: "fakeurl.com")!)
     
     override func setUp() {
         super.setUp()
@@ -29,10 +29,11 @@ class SandPApiTests: XCTestCase {
         sut = MockSandPApi()
         testObj = [Symbol]()
         testObj.append(foo1)
-        stub(condition: isHost(url)) { _ in
+        
+        stub(condition: isHost("www.SandP.com")) { _ in
             return OHHTTPStubsResponse(jsonObject: self.testObj, statusCode: 200, headers: nil)
         }
-        stub(condition: isHost(fakeUrl)) { _ in
+        stub(condition: isHost("fakeurl.com")) { _ in
             return OHHTTPStubsResponse(error: RxAlamofireUnknownError)
         }
     }
@@ -43,22 +44,20 @@ class SandPApiTests: XCTestCase {
     }
     
     func testData() {
-        let observable = data(.get, url)
-            .toBlocking()
-            .firstOrNil()
+        let observable = URLSession.shared.rx.data(request: self.request)
         
         expect(observable).notTo(beNil())
     }
     
     func testError() {
         var erroredCorrectly = false
-        let observable = data(.get, fakeUrl)
+        let observable = URLSession.shared.rx.json(request: self.errorRequest)
         
         do {
             let _ = try observable.toBlocking().first()
             assertionFailure()
         } catch {
-             erroredCorrectly = true
+            erroredCorrectly = true
         }
         
         expect(erroredCorrectly) == true
