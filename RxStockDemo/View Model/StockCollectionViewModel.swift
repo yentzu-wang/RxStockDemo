0936e9740c7ded0d477205b72736341275aac0ec
Frank <frank7300228@gmail.com>
2017-11-21 19:03:24 +0800

    Remove strong reference

--- RxStockDemo/View Model/StockCollectionViewModel.swift
+++ RxStockDemo/View Model/StockCollectionViewModel.swift
@@ -11,19 +11,20 @@ import RxSwift
 import RealmSwift
 
 class StockCollectionViewModel {
+    private let bag = DisposeBag()
+    
     func subscriptNewestPrice(symbol: String, interval: QueryInterval) -> PublishSubject<StockPrice> {
-        let bag = DisposeBag()
         let subject = PublishSubject<StockPrice>()
-        
         let timer = Observable<Int>.interval(15, scheduler: MainScheduler.instance)
+        
         timer
             .startWith(-1)
-            .subscribe { _ in
+            .subscribe { [unowned self] _ in
                 StocksApi.shared.intraDayNewestQuery(symbol: symbol, interval: interval)
                     .subscribe(onNext: { (stockPrice) in
                         subject.onNext(stockPrice)
                     })
-                    .disposed(by: bag)
+                    .disposed(by: self.bag)
             }
             .disposed(by: bag)
         
