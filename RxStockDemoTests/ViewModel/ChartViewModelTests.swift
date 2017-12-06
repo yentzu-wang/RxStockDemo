38b4a7fe0454b1f398fe91be79fcfefaadd5ce51
Frank <frank7300228@gmail.com>
2017-12-06 18:47:38 +0800

    Update test cases

--- RxStockDemoTests/ViewModel/ChartViewModelTests.swift
+++ RxStockDemoTests/ViewModel/ChartViewModelTests.swift
@@ -7,16 +7,44 @@
 //
 
 import XCTest
+import RxSwift
+import RxBlocking
+import Nimble
+import RxNimble
+import RealmSwift
+import RxSwift
+import RxRealm
+@testable import RxStockDemo
 
 class ChartViewModelTests: XCTestCase {
+    var sut: ChartViewModelProtocol!
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        sut = MockChartViewModelProtocol()
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    func testPricesNotNil() {
+        expect(self.sut.prices).notTo(beNil())
+    }
+}
+
+extension ChartViewModelTests {
+    class MockChartViewModelProtocol: ChartViewModelProtocol {
+        var prices: Observable<(AnyRealmCollection<StockPrice>, RealmChangeset?)>
+        
+        init() {
+            let realm = try! Realm(configuration: Realm.Configuration(inMemoryIdentifier: "InMemoryRealmDailyTesting"))
+            let result = realm.objects(StockPrice.self)
+                .filter("symbol = 'foo'")
+                .sorted(byKeyPath: "date", ascending: true)
+            prices = Observable.changeset(from: result)
+        }
     }
 }
